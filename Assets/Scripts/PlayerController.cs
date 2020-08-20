a6ec02437a8b46eea848893089f0f1e5957089b4
Matteo <matteo@tntc.it>
2020-08-20 18:01:14 +0200

    Project push

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public ParticleSystem snakeSpell, aoeSnakeSpell;
+    public Transform singleParticlePos, aoeParticlePos;
+
+    Animator animController;
+    private void Start()
+    {
+        animController = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) 
+            animController.SetTrigger("SingleSpell");
+        if (Input.GetKeyDown(KeyCode.Q))
+            animController.SetTrigger("AOESpell");
+    }
+
+    public void SingleSnakeSpell()
+    {
+        Destroy(Instantiate(snakeSpell.gameObject, singleParticlePos.position, singleParticlePos.rotation).GetComponent<ParticleSystem>(), 8);
+    }
+
+    public void AOESnakeSpell()
+    {
+        Destroy(Instantiate(aoeSnakeSpell.gameObject, aoeParticlePos.position, aoeParticlePos.rotation).GetComponent<ParticleSystem>(), 8);
+    }
+
+}
